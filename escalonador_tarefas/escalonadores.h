2c7f2053088dc6e1c9f8ce762d4e0840bd10a702
Pedro Militão <pepmilitao@gmail.com>
2025-10-21 08:44:12 -0300

    Modificados parâmetros das funções de escalonadores

--- escalonador_tarefas/escalonadores.h
+++ escalonador_tarefas/escalonadores.h
@@ -1,20 +1,20 @@
 // First Come, First Served
-int* fcfs(int** tasks, int qtd_task);
+int* fcfs(int* tasks, int qtd_task);
 
 // Shortest Job First
-int* sjf(int** tasks, int qtd_task);
+int* sjf(int* tasks, int qtd_task);
 
 // Shortest Remaining Time First
-int* srtf(int** tasks, int qtd_task);
+int* srtf(int* tasks, int qtd_task);
 
 // Prioridade cooperativo
-int* prioc(int** tasks, int qtd_taskd);
+int* prioc(int* tasks, int qtd_taskd);
 
 // Prioridade preemptivo
-int* priop(int** tasks, int qtd_taskd);
+int* priop(int* tasks, int qtd_taskd);
 
 // Round-Robin clássico (sem prioridade)
-int* rr(int** tasks, int qtd_taskd, int quantum);
+int* rr(int* tasks, int qtd_taskd, int quantum);
 
 // Round-Robin com prioridade e aging
-int* rr_d(int** tasks, int qtd_taskd, int quantum, int alfa);
+int* rr_d(int* tasks, int qtd_taskd, int quantum, int alfa);
