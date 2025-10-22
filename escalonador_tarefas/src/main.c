39974c4aaff518852c06a8d27eac7061b3b7aaff
Luiza Martins <luiza.martins@asaas.com.br>
2025-10-21 23:13:36 -0300

    Adiciona observação de leitura de config na main

--- escalonador_tarefas/src/main.c
+++ escalonador_tarefas/src/main.c
@@ -4,4 +4,18 @@
 
 int main() {
     // TODO
+    // Lembrar de ler o arquivo de config.txt
+    // Exemplo:
+//    void ler_config(int *quantum, int *aging) {
+//        FILE *fp = fopen("config.txt", "r");
+//        if (!fp) {
+//            printf("Erro ao abrir config.txt\n");
+//            exit(1);
+//        }
+//        char linha[100];
+//        fgets(linha, sizeof(linha), fp);
+//        fclose(fp);
+//
+//        // Esperado: quantum:2 aging:1
+//        sscanf(linha, "quantum:%d aging:%d", quantum, aging);
 }
